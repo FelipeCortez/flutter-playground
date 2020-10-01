d543532ccce2402ec50bb49aafb1f9f1dbc47751
Felipe Cortez <felipecortezfi@gmail.com>
2020-10-01 20:49:00 -0300

    Refactor

--- lib/mirror.dart
+++ lib/mirror.dart
@@ -9,71 +9,101 @@ class Model {
 
   List<int> listOfNumbers;
   List<List<int>> listOfListOfNumbers;
-  Set<int> setOfNumbers;
+  Map<int, List<bool>> mapFromIntToList;
+  HashSet<int> setOfNumbers;
   bool tOrF;
   int noDot;
   double decimal;
   String text;
   Symbol sym;
+
+  MiniThing mini;
+
+  @override
+  String toString() {
+    return "<Model> { mini: $mini\n"
+           "        , listOfNumbers: $listOfNumbers\n"
+           "        , listOfListOfNumbers: $listOfListOfNumbers\n"
+           "        , mapFromIntToList: $mapFromIntToList\n"
+           "        , setOfNumbers: $setOfNumbers\n"
+           "        , tOrF: $tOrF\n"
+           "        , noDot: $noDot\n"
+           "        , decimal: $decimal\n"
+           "        , text: $text\n"
+           "        , sym: $sym\n"
+           "        }";
+  }
 }
 
 class MiniThing {
-  int thingy;
+  bool thingy;
+
+  @override
+  String toString() {
+    return "<MiniThing> {thingy: $thingy}";
+  }
 }
 
 var resolvers = {
-  #dart.core.String: () => "Hello",
-  #dart.core.int   : () => 12345,
-  #dart.core.double: () => 123.45,
-  #dart.core.bool  : () => false,
-  #dart.core.Symbol: () => #sym,
-  #dart.core.Set   : (TypeMirror x) => createSetOf(x),
-  #dart.core.List  : (TypeMirror x) => createListOf(x),
+#dart.core.String: () => "Hello",
+#dart.core.int   : () => 12345,
+#dart.core.double: () => 123.45,
+#dart.core.bool  : () => false,
+#dart.core.Symbol: () => #sym,
+#dart.core.Map   : (TypeMirror key, TypeMirror value) => createMapOf(key, value),
+#dart.core.Set   : (TypeMirror type) => createSetOf(type),
+#dart.core.List  : (TypeMirror type) => createListOf(type),
 };
 
-dynamic resolveType(TypeMirror x) {
-  return Function.apply(resolvers[x.qualifiedName], x.typeArguments);
+dynamic resolveType(TypeMirror type) {
+  var resolver = resolvers[type.qualifiedName];
+
+  if (resolver != null) {
+    return Function.apply(resolver, type.typeArguments);
+  }
+
+  return fillPrime(type as ClassMirror);
 }
 
-List<dynamic> createListOf(TypeMirror x) {
-  var list = (reflectType(List, [x.reflectedType]) as ClassMirror)
-      .newInstance(Symbol(''), []).reflectee;
-  list.add(resolveType(x));
-  list.add(resolveType(x));
+T create<T>(List<Type> typeParameters) {
+  return (reflectType(T, typeParameters) as ClassMirror).newInstance(Symbol(''), []).reflectee as T;
+}
+
+List<dynamic> createListOf(TypeMirror type) {
+  var list = create<List>([type.reflectedType]);
+  list.add(resolveType(type));
+  list.add(resolveType(type));
   return list;
 }
 
-Set<dynamic> createSetOf(TypeMirror x) {
-  var zet = (reflectType(LinkedHashSet, [x.reflectedType]) as ClassMirror)
-      .newInstance(Symbol(''), []).reflectee;
-  zet.add(resolveType(x));
+Set<dynamic> createSetOf(TypeMirror type) {
+  var zet = create<LinkedHashSet>([type.reflectedType]);
+  zet.add(resolveType(type));
   return zet;
 }
 
-T fill<T>() {
-  var clazz = reflectClass(T);
+Map<dynamic, dynamic> createMapOf(TypeMirror keyType, TypeMirror valueType) {
+  var map = create<Map>([keyType.reflectedType, valueType.reflectedType]);
+  map[resolveType(keyType)] = resolveType(valueType);
+  return map;
+}
 
+dynamic fillPrime(ClassMirror clazz) {
   var variables = clazz.declarations.values
-      .where((x) => x is VariableMirror)
-      .map((x) => x as VariableMirror);
+      .where((type) => type is VariableMirror)
+      .map((type) => type as VariableMirror);
 
   var mirror = clazz.newInstance(Symbol(''), []);
 
-  variables.forEach((variable) =>
-  mirror.setField(variable.simpleName, resolveType(variable.type)));
+  variables.forEach((variable) => mirror.setField(variable.simpleName, resolveType(variable.type)));
 
-  return mirror.reflectee as T;
+  return mirror.reflectee;
+}
+
+T fill<T>() {
+  return fillPrime(reflectClass(T)) as T;
 }
 
 main() {
-  var filledModel = fill<Model>();
-
-  print(filledModel.listOfListOfNumbers);
-  print(filledModel.listOfNumbers);
-  print(filledModel.setOfNumbers);
-  print(filledModel.noDot);
-  print(filledModel.decimal);
-  print(filledModel.text);
-  print(filledModel.tOrF);
-  print(filledModel.sym);
+  print(fill<Model>());
 }
